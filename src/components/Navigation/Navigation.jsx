a6ee5c01d3f124e1c253903476effd4e4d04c5db
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-02-24 12:24:14 +0530

    add primary navigation

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+      <p>Sign Out</p>
+    </nav>
+  );
+}
+
+export default Navigation;
